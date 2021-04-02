958e3cb6372937414c69a7ef6a011a77965b66a0
Ivan <vanyablya003@gmail.com>
2021-04-02 23:13:09 +0300

    дополнение

--- Задача5/task5.js
+++ Задача5/task5.js
@@ -1,2 +1,5 @@
 const fs = require("fs")
 
+let task5File_List = fs.readFileSync("./Task5-txtlist.txt", "utf-8").toString()
+let task5File_Adr = fs.readFileSync("./Task5-adr.txt", "utf-8").toString()
+
