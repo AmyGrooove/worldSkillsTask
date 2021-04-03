dc2fbcabb0651a9f4378a3b6a5ac9b401384bcdd
TIdjekey <kicha9men@gmail.com>
2021-04-03 13:38:19 +0300

    added npm scripts

--- Задача1/task1.js
+++ Задача1/task1.js
@@ -1,7 +1,7 @@
 const cryptoJS = require("crypto-js")
 const fs = require("fs")
 
-let task1File = fs.readFileSync("./Task1-tx.txt", "utf-8").split(" ").join("").split("\r\n").join("").toString()
+let task1File = fs.readFileSync("./Задача1/Task1-tx.txt", "utf-8").split(" ").join("").split("\r\n").join("").toString()
 
 task1File = cryptoJS.MD5(task1File).toString()
 
